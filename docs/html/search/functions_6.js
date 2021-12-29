bb137fca497d82d1969b3b8b2effe723ec1edb73
nogueira <nogueira@DESKTOP-P944B51>
2021-12-29 18:42:59 +0000

    doxygen changes

--- docs/html/search/functions_6.js
+++ docs/html/search/functions_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['padding_0',['padding',['../classbitstream.html#aa2d1445ffe50215291edc0000a292cc1',1,'bitstream']]],
-  ['predictive_5fcoding_1',['predictive_coding',['../classImageCodec.html#ab25d13565847b2dddb785e35a34a144a',1,'ImageCodec']]],
-  ['predictive_5fdecoding_2',['predictive_decoding',['../classImageCodec.html#a5750c21638fce05ccbd351dd1bd96d78',1,'ImageCodec']]]
+  ['predictive_5fcoding_1',['predictive_coding',['../classAudioCodec.html#a554c196e4d5b175f5081b73619d2ba3a',1,'AudioCodec::predictive_coding()'],['../classImageCodec.html#ab25d13565847b2dddb785e35a34a144a',1,'ImageCodec::predictive_coding()']]],
+  ['predictive_5fdecoding_2',['predictive_decoding',['../classAudioCodec.html#a773debc048244b89d51ec8101636a089',1,'AudioCodec::predictive_decoding()'],['../classImageCodec.html#a5750c21638fce05ccbd351dd1bd96d78',1,'ImageCodec::predictive_decoding()']]]
 ];
